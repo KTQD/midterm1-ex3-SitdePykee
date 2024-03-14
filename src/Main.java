703af0dc8cecc08c4cf848fa7beac268216a0208
SitdePykee <129248580+SitdePykee@users.noreply.github.com>
2024-03-14 08:33:11 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        PrintWriter writer = new PrintWriter(new FileWriter("output.txt"));
+        String line;
+        while ((line = reader.readLine()) != null){
+            line = line.replaceAll("Nha Trang", "Vũng Tàu");
+            writer.println(line);
 
-
-        Bài làm viết tại đây
-
+        }
     }
 }
